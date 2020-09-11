0cbe17844435880dc987994c2ca4974846700915
Muhammad Abdullah <muhammad-abdullah1@outlook.com>
2020-09-10 21:20:17 -0600

    Css changes Changes css menu layout, font, and font color

--- src/index.js
+++ src/index.js
@@ -31,21 +31,28 @@ for (var menuIndex in sortedMenu) {
 function createList(menuitem, menuType) {
   var foodlist = document.createElement("li"); // Create a <li> node
   var eachFood = document.createElement("ul"); // Create a <ul> node
+  var foodTitle = document.createElement("b"); // Crete a <b> node
 
   var nameNode = document.createTextNode(sortedMenu[menuitem].name); // Create a name node
-  nameNode.className = "foodName";
+  foodTitle.appendChild(nameNode);
+  // foodlist.style.textDecoration = "underline";
 
   var descNode = document.createTextNode(sortedMenu[menuIndex].description); // Create a description node
   var desc = document.createElement("li");
+  desc.id = "description";
   desc.appendChild(descNode);
 
   var priceNode = document.createTextNode(
     "Price: " + sortedMenu[menuitem].price.toFixed(2)
   ); // Create a price node
   var price = document.createElement("li");
-  price.appendChild(priceNode);
+  var boldprice = document.createElement("b");
+  boldprice.style.color = "#AA913F";
 
-  foodlist.appendChild(nameNode);
+  boldprice.appendChild(priceNode);
+  price.appendChild(boldprice);
+
+  foodlist.appendChild(foodTitle);
   foodlist.appendChild(eachFood);
   eachFood.appendChild(desc);
   eachFood.appendChild(price);
